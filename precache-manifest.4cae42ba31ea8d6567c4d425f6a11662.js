8324349067c6c9c2be7684bb8c1d70aa0bdeec26
Pawan Gowda <38250665+pa1kumarbh@users.noreply.github.com>
2020-03-19 19:05:40 +0530

    Add files via upload

---
+++ precache-manifest.4cae42ba31ea8d6567c4d425f6a11662.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fdee05b5586864eef08912a981aaa47",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34b0915c5bbcbcf7567f",
+    "url": "/static/css/main.ecd1933b.chunk.css"
+  },
+  {
+    "revision": "847065f1805955ef2da4",
+    "url": "/static/js/2.785b9133.chunk.js"
+  },
+  {
+    "revision": "e1f6455b5696e672eb99ff7a82d125da",
+    "url": "/static/js/2.785b9133.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34b0915c5bbcbcf7567f",
+    "url": "/static/js/main.60b119d5.chunk.js"
+  },
+  {
+    "revision": "1e4bcf8a7f65a1ade486",
+    "url": "/static/js/runtime-main.0fa65764.js"
+  },
+  {
+    "revision": "64b22be27e056c743bb3c9ebd20a9092",
+    "url": "/static/media/bground.64b22be2.jpg"
+  },
+  {
+    "revision": "c72188dd16a581a3c6ab875653a0c653",
+    "url": "/static/media/img.c72188dd.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a8466d0e9de4b735918c0b3399b5c19a",
+    "url": "/static/media/logo2.a8466d0e.jpg"
+  },
+  {
+    "revision": "355c962e1d00dd28e9e938ec90364db7",
+    "url": "/static/media/wall.355c962e.jpg"
+  },
+  {
+    "revision": "9f2e8a3b05ad061d525bee88cd246394",
+    "url": "/static/media/zurich.9f2e8a3b.jpg"
+  }
+]);
